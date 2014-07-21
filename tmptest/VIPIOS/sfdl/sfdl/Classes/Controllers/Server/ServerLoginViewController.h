779aba3693154b417a5003dd3c2f79b4ab832d8c
allen.wang <allen.wang@b5m.com>
2014-07-21 17:56:15 +0800

    commit for server

---
+++ tmptest/VIPIOS/sfdl/sfdl/Classes/Controllers/Server/ServerLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  ServerLoginViewController.m.h
+//  sport
+//
+//  Created by allen.wang on 5/20/14.
+//  Copyright (c) 2014 allen.wang. All rights reserved.
+//
+
+#import "BaseTitleViewController.h"
+
+@interface ServerLoginViewController : BaseTitleViewController
+
+@end
